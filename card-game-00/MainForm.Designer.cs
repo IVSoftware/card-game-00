bd03c577987bb6cc33f2432db63d5ca1286767f5
Tom Gregor <tgregor@ivsoftware.com>
2022-12-09 09:25:38 -0700

    Add project files.

---
+++ card-game-00/MainForm.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace card_game_00
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutCards = new System.Windows.Forms.TableLayoutPanel();
+            this.labelHandA1 = new System.Windows.Forms.Label();
+            this.labelHandA2 = new System.Windows.Forms.Label();
+            this.labelHandA3 = new System.Windows.Forms.Label();
+            this.labelHandA4 = new System.Windows.Forms.Label();
+            this.labelHandA5 = new System.Windows.Forms.Label();
+            this.labelHandB1 = new System.Windows.Forms.Label();
+            this.labelHandB2 = new System.Windows.Forms.Label();
+            this.labelHandB3 = new System.Windows.Forms.Label();
+            this.labelHandB4 = new System.Windows.Forms.Label();
+            this.labelHandB5 = new System.Windows.Forms.Label();
+            this.buttonDeal = new System.Windows.Forms.Button();
+            this.tableLayoutCards.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutCards
+            // 
+            this.tableLayoutCards.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutCards.ColumnCount = 5;
+            this.tableLayoutCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutCards.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutCards.Controls.Add(this.labelHandA1, 0, 0);
+            this.tableLayoutCards.Controls.Add(this.labelHandA2, 1, 0);
+            this.tableLayoutCards.Controls.Add(this.labelHandA3, 2, 0);
+            this.tableLayoutCards.Controls.Add(this.labelHandA4, 3, 0);
+            this.tableLayoutCards.Controls.Add(this.labelHandA5, 4, 0);
+            this.tableLayoutCards.Controls.Add(this.labelHandB1, 0, 2);
+            this.tableLayoutCards.Controls.Add(this.labelHandB2, 1, 2);
+            this.tableLayoutCards.Controls.Add(this.labelHandB3, 2, 2);
+            this.tableLayoutCards.Controls.Add(this.labelHandB4, 3, 2);
+            this.tableLayoutCards.Controls.Add(this.labelHandB5, 4, 2);
+            this.tableLayoutCards.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tableLayoutCards.Location = new System.Drawing.Point(41, 12);
+            this.tableLayoutCards.Name = "tableLayoutCards";
+            this.tableLayoutCards.RowCount = 3;
+            this.tableLayoutCards.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutCards.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutCards.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutCards.Size = new System.Drawing.Size(394, 320);
+            this.tableLayoutCards.TabIndex = 0;
+            // 
+            // labelHandA1
+            // 
+            this.labelHandA1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandA1.AutoSize = true;
+            this.labelHandA1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandA1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandA1.Location = new System.Drawing.Point(3, 0);
+            this.labelHandA1.Name = "labelHandA1";
+            this.labelHandA1.Size = new System.Drawing.Size(72, 140);
+            this.labelHandA1.TabIndex = 0;
+            this.labelHandA1.Text = "*";
+            this.labelHandA1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandA2
+            // 
+            this.labelHandA2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandA2.AutoSize = true;
+            this.labelHandA2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandA2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandA2.Location = new System.Drawing.Point(81, 0);
+            this.labelHandA2.Name = "labelHandA2";
+            this.labelHandA2.Size = new System.Drawing.Size(72, 140);
+            this.labelHandA2.TabIndex = 0;
+            this.labelHandA2.Text = "*";
+            this.labelHandA2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandA3
+            // 
+            this.labelHandA3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandA3.AutoSize = true;
+            this.labelHandA3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandA3.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandA3.Location = new System.Drawing.Point(159, 0);
+            this.labelHandA3.Name = "labelHandA3";
+            this.labelHandA3.Size = new System.Drawing.Size(72, 140);
+            this.labelHandA3.TabIndex = 0;
+            this.labelHandA3.Text = "*";
+            this.labelHandA3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandA4
+            // 
+            this.labelHandA4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandA4.AutoSize = true;
+            this.labelHandA4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandA4.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandA4.Location = new System.Drawing.Point(237, 0);
+            this.labelHandA4.Name = "labelHandA4";
+            this.labelHandA4.Size = new System.Drawing.Size(72, 140);
+            this.labelHandA4.TabIndex = 0;
+            this.labelHandA4.Text = "*";
+            this.labelHandA4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandA5
+            // 
+            this.labelHandA5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandA5.AutoSize = true;
+            this.labelHandA5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandA5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandA5.Location = new System.Drawing.Point(315, 0);
+            this.labelHandA5.Name = "labelHandA5";
+            this.labelHandA5.Size = new System.Drawing.Size(76, 140);
+            this.labelHandA5.TabIndex = 0;
+            this.labelHandA5.Text = "*";
+            this.labelHandA5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandB1
+            // 
+            this.labelHandB1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandB1.AutoSize = true;
+            this.labelHandB1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandB1.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandB1.Location = new System.Drawing.Point(3, 180);
+            this.labelHandB1.Name = "labelHandB1";
+            this.labelHandB1.Size = new System.Drawing.Size(72, 140);
+            this.labelHandB1.TabIndex = 0;
+            this.labelHandB1.Text = "*";
+            this.labelHandB1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandB2
+            // 
+            this.labelHandB2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandB2.AutoSize = true;
+            this.labelHandB2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandB2.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandB2.Location = new System.Drawing.Point(81, 180);
+            this.labelHandB2.Name = "labelHandB2";
+            this.labelHandB2.Size = new System.Drawing.Size(72, 140);
+            this.labelHandB2.TabIndex = 0;
+            this.labelHandB2.Text = "*";
+            this.labelHandB2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandB3
+            // 
+            this.labelHandB3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandB3.AutoSize = true;
+            this.labelHandB3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandB3.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandB3.Location = new System.Drawing.Point(159, 180);
+            this.labelHandB3.Name = "labelHandB3";
+            this.labelHandB3.Size = new System.Drawing.Size(72, 140);
+            this.labelHandB3.TabIndex = 0;
+            this.labelHandB3.Text = "*";
+            this.labelHandB3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandB4
+            // 
+            this.labelHandB4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandB4.AutoSize = true;
+            this.labelHandB4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandB4.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandB4.Location = new System.Drawing.Point(237, 180);
+            this.labelHandB4.Name = "labelHandB4";
+            this.labelHandB4.Size = new System.Drawing.Size(72, 140);
+            this.labelHandB4.TabIndex = 0;
+            this.labelHandB4.Text = "*";
+            this.labelHandB4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelHandB5
+            // 
+            this.labelHandB5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelHandB5.AutoSize = true;
+            this.labelHandB5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelHandB5.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelHandB5.Location = new System.Drawing.Point(315, 180);
+            this.labelHandB5.Name = "labelHandB5";
+            this.labelHandB5.Size = new System.Drawing.Size(76, 140);
+            this.labelHandB5.TabIndex = 0;
+            this.labelHandB5.Text = "*";
+            this.labelHandB5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonDeal
+            // 
+            this.buttonDeal.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDeal.Location = new System.Drawing.Point(454, 298);
+            this.buttonDeal.Name = "buttonDeal";
+            this.buttonDeal.Size = new System.Drawing.Size(112, 34);
+            this.buttonDeal.TabIndex = 1;
+            this.buttonDeal.Text = "Deal";
+            this.buttonDeal.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 344);
+            this.Controls.Add(this.buttonDeal);
+            this.Controls.Add(this.tableLayoutCards);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            this.tableLayoutCards.ResumeLayout(false);
+            this.tableLayoutCards.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutCards;
+        private System.Windows.Forms.Button buttonDeal;
+        private System.Windows.Forms.Label labelHandA1;
+        private System.Windows.Forms.Label labelHandA2;
+        private System.Windows.Forms.Label labelHandA3;
+        private System.Windows.Forms.Label labelHandA4;
+        private System.Windows.Forms.Label labelHandA5;
+        private System.Windows.Forms.Label labelHandB1;
+        private System.Windows.Forms.Label labelHandB2;
+        private System.Windows.Forms.Label labelHandB3;
+        private System.Windows.Forms.Label labelHandB4;
+        private System.Windows.Forms.Label labelHandB5;
+    }
+}
+
---
+++ card-game-00/MainForm.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace card_game_00
+{   
+    public enum CardValue
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13
+    }
+    public enum CardSuit
+    {
+        Hearts = 1,
+        Spades = 2,
+        Clubs = 3,
+        Diamonds = 4
+    }
+    public partial class MainForm : Form
+    {
+        // Make instance of CardDeck
+        Deck Deck = new Deck();
+        public MainForm()
+        {
+            InitializeComponent();
+            tableLayoutCards.Font = new Font("Sergoe UI Symbol", 9);
+            // Static or const members of a class do not require an
+            // instance. Use the class name to reference these members.
+            labelHandA1.Text = $"10 {Card.Spades}";
+            labelHandA2.Text = $"J {Card.Spades}";
+            labelHandA3.Text = $"Q {Card.Spades}";
+            labelHandA4.Text = $"K {Card.Spades}";
+            labelHandA5.Text = $"A {Card.Spades}";
+            labelHandB1.Text = $"10 {Card.Hearts}";
+            labelHandB2.Text = $"J {Card.Hearts}";
+            labelHandB3.Text = $"Q {Card.Hearts}";
+            labelHandB4.Text = $"K {Card.Hearts}";
+            labelHandB5.Text = $"A {Card.Hearts}";
+            buttonDeal.Click += dealTheCards;
+        }
+
+        private async void dealTheCards(object sender, EventArgs e)
+        {
+            UseWaitCursor = true;
+            // When a non-UI task might take some time, run it on a Task.
+            await Task.Run(() => Deck.Shuffle());
+            labelHandA1.Text = Deck.Dequeue().ToString();
+            labelHandA2.Text = Deck.Dequeue().ToString();
+            labelHandA3.Text = Deck.Dequeue().ToString();
+            labelHandA4.Text = Deck.Dequeue().ToString();
+            labelHandA5.Text = Deck.Dequeue().ToString();
+            labelHandB1.Text = Deck.Dequeue().ToString(); ;
+            labelHandB2.Text = Deck.Dequeue().ToString();
+            labelHandB3.Text = Deck.Dequeue().ToString();
+            labelHandB4.Text = Deck.Dequeue().ToString();
+            labelHandB5.Text = Deck.Dequeue().ToString();
+            UseWaitCursor = false;
+        }
+    }
+    public class Card
+    {
+        // https://office-watch.com/2021/playing-card-suits-%E2%99%A0%E2%99%A5%E2%99%A6%E2%99%A3-in-word-excel-powerpoint-and-outlook/#:~:text=Insert%20%7C%20Symbols%20%7C%20Symbol%20and%20look,into%20the%20character%20code%20box.
+        public const string Hearts = "\u2665";
+        public const string Spades = "\u2660";
+        public const string Clubs = "\u2663";
+        public const string Diamonds = "\u2666";
+        public CardValue CardValue { get; set; }
+        public CardSuit CardSuit { get; set; }
+        public override string ToString()
+        {
+            string value, suit = null;
+            switch (CardValue)
+            {
+                case CardValue.Ace: value = "A"; break;
+                case CardValue.Jack: value = "J"; break;
+                case CardValue.Queen:  value = "Q"; break;
+                case CardValue.King: value = "K"; break;
+                default: value = $"{(int)CardValue}"; break;
+            }
+            switch (CardSuit)
+            {
+                case CardSuit.Hearts: suit = Hearts; break;
+                case CardSuit.Spades: suit = Spades; break;
+                case CardSuit.Clubs: suit = Clubs; break;
+                case CardSuit.Diamonds: suit = Diamonds ; break;
+            }
+            return $"{value} {suit}";
+        }
+    }
+    public class Deck : Queue<Card>
+    {
+        private readonly Random _rando = new Random();
+        private readonly Card[] _unshuffled;
+        public Deck()
+        {
+            List<Card> tmp = new List<Card>();
+            foreach(CardValue value in Enum.GetValues(typeof(CardValue)))
+            {
+                foreach (CardSuit suit in Enum.GetValues(typeof(CardSuit)))
+                {
+                    tmp.Add(new Card { CardValue = value, CardSuit = suit });
+                }
+            }
+            _unshuffled = tmp.ToArray();
+        }
+        public void Shuffle()
+        {
+            Clear();
+            List<int> sequence = Enumerable.Range(0, 52).ToList();
+            while(sequence.Count != 0)
+            {
+                int nextRand = _rando.Next(0, sequence.Count());
+                Enqueue(_unshuffled[nextRand]);
+                sequence.RemoveAt(nextRand);
+            }
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+        public Card GetNext() => Dequeue();
+    }
+}
